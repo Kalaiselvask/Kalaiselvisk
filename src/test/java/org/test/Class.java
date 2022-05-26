49849dfb4927b6f6dce9066726945f0aac1bf9f0
Kalaiselvisk <github>
2022-05-26 14:34:40 +0530

    Kalaiselvi

---
+++ src/test/java/org/test/Class.java
@@ -0,0 +1,17 @@
+package org.test;
+
+public class Class {
+	
+	private void test1() {
+		System.out.println("Test1");
+	}
+private void test2() {
+ System.out.println("Test2");
+}
+private void test3() {
+System.out.println("Test3");
+}
+private void test4() {
+System.out.println("Test4");
+}
+}
